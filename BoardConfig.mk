d17ca1c394f80cfb8a794766b263cf4e9b0a8b92
Jacobson <zkyprojekt@gmail.com>
2023-04-25 13:09:10 +0000

    miatoll: BoardConfig: Use default AOSP clang for building kernel
    Revert "miatoll: BoardConfig.mk: Use trb clang for building kernel"
    
    This reverts commit fbc0d1b0d26b9176ec345c338a6887b6c9c260ac.
    
    Revert "miatoll: BoardConfig: Build kernel with clang r487747"
    
    This reverts commit 3dfe51688169ab8db7e75d82dbad1cf8bec70287.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,11 +107,6 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump LLVM_IAS=1
 TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
 TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := trb_clang
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(TARGET_KERNEL_CLANG_PATH)/bin/aarch64-linux-gnu-
-TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(TARGET_KERNEL_CLANG_PATH)/bin/arm-linux-gnueabi-
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
